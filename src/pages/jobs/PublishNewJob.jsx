39627d4fe153fe99821f85f6c1164303124cd39f
sayed8901 <sayed91515@gmail.com>
2024-10-04 23:37:19 +0600

    Employment Status field modified in the publish new job form

--- src/pages/jobs/PublishNewJob.jsx
+++ src/pages/jobs/PublishNewJob.jsx
@@ -89,6 +89,8 @@ const PublishNewJob = () => {
       salary: parseInt(formData.salary),
     };
 
+    console.log(jobPublishData);
+
     fetch(
       `${
         import.meta.env.VITE_API_URL
@@ -254,6 +256,9 @@ const PublishNewJob = () => {
                     onChange={handleChange}
                     className="block w-full rounded-md border-0 py-3 px-5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6 bg-white"
                   >
+                    <option value="" disabled>
+                      Select Employment Status
+                    </option>
                     <option value="permanent">Permanent</option>
                     <option value="contractual">Contractual</option>
                     <option value="part-time">Part-time</option>
